787385706b84a478f69c1c164ebd7522f2e4f52b
taortega7 <taortega@estudiante.uniajc.edu.co>
2024-09-30 20:37:04 +0000

    Finalizacion codigo 5.6

--- CLASE30/09/24/Ejercicio55.java
+++ CLASE30/09/24/Ejercicio55.java
@@ -1,7 +1,27 @@
+import java.util.Scanner; 
 public class Ejercicio55 {
     public static void main(String[] args) {
-        
-        System.out.println("Mi primer programa de JAVA en Github");
+        double nota= 0.0;
+        int aprobados  = 0,noAprobados = 0;
+        //notas de los estudiantes
+Scanner leer = new Scanner(System.in);
+System.out.println("Digite la cantidad de estudiantes: ");
+int estudiantes = leer.nextInt();
+
+
+for(int i = 1; i <= estudiantes; i++){
+    System.out.println("Nota del estudiante "+ i +":");
+    nota = leer.nextDouble();
+
+    if (nota >= 5.0){
+        aprobados++;
+    }else{
+        noAprobados++;
+
     }
     
+}
+System.out.println("Aprobados: " + aprobados + "No Aprobados: " + noAprobados);
+
+}
 }
