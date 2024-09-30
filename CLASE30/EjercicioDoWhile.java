5084c8db213da158bf319a5b8f4d73192cfcc1b2
taortega7 <taortega@estudiante.uniajc.edu.co>
2024-09-30 21:23:53 +0000

    Explicacin DoWhile

---
+++ CLASE30/EjercicioDoWhile.java
@@ -0,0 +1,32 @@
+package CLASE30;
+
+public class EjercicioDoWhile {
+    public static void main(String[] args) {
+        /*
+         * Do While
+         */
+        String cad = "Programacion I";
+        int j = 1;
+        do {
+            System.out.println(cad);
+            j++;
+        } while (j < 5);
+
+        System.out.println("*******************");
+        /*
+         * while / for
+         */
+        int l = 1;
+        while (l < 5) {
+            System.out.println(cad);
+            l++;
+        }
+
+        System.out.println("****************");
+
+        for (int i = 1; i < 5; i++) {
+            System.out.println(cad);
+        }
+
+    }
+}
