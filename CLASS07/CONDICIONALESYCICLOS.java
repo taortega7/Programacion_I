276bdec3e09eebb4d5f6d6a2dccad20067db9b3c
Tati <taortega@estudiante.uniajc.edu.co>
2024-10-07 20:50:02 +0000

    indiavspakistan

--- CLASS07/CONDICIONALESYCICLOS.java
+++ CLASS07/CONDICIONALESYCICLOS.java
@@ -1,14 +1,12 @@
 public class CONDICIONALESYCICLOS {
     public static void main(String[] args) {
 
-        int marks = 80;
-        if (marks > 70)
-            System.out.println("Distinction");
-        if (marks > 35)
-            System.out.println("Pass");
-        else
-            System.out.println("Fail");
-        System.out.println("Better luck next time");
-    }
-
+        int india_score = 300, pakistan_score = 290;
+        if (india_score > pakistan_score)
+        System.out.println("India Wins");
+        else 
+        System.out.println("Pakistan Wins");
+   
+    System.out.println(india_score > pakistan_score ? "India Wins" : "Pakistan Wins");
+}
 }
