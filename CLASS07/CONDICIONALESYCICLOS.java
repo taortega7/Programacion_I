9f53cd6c32fec6845b0fa57b0ee25b9e90215dd3
Tati <taortega@estudiante.uniajc.edu.co>
2024-10-07 19:44:42 +0000

    INICIO DE CONDICI...

--- CLASS07/CONDICIONALESYCICLOS.java
+++ CLASS07/CONDICIONALESYCICLOS.java
@@ -1,5 +1,10 @@
+public class CONDICIONALESYCICLOS  {
+    public static void main(String[] args) {
+
+boolean a=b=c=true;
+
 if(!a || (b&&c)){
     System.out.println("Sentencia if ejecutada");
 }else{
-    System.out.println("Sentencia else ejecutada")
+    System.out.println("Sentencia else ejecutada");
 }
